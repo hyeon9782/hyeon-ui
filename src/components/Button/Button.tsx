78cf688141cad729e8f9ae3c97605efa73b42dcb
hyeon9782 <hyeon9782@gmail.com>
2023-12-26 20:38:39 +0900

    ♻️ refactor: Button 컴포넌트 리팩토링

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,16 @@
-type Props = {
-  text: string;
-};
-const Button = ({ text }: Props) => {
-  return <div>{text}</div>;
+import React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+}
+const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
+  const colorClass = variant === "primary" ? "blue" : "red";
+  return (
+    <button style={{ backgroundColor: colorClass }} {...props}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
